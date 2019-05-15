8fdd792f4ecdb242e55a0fd3b13b27828c955395
Guillaume Dufour <guillaume.duff@gmail.com>
2019-05-15 21:09:12 +0200

    read form value

--- assets/javascript/generator.js
+++ assets/javascript/generator.js
@@ -1,4 +1,27 @@
 function generate() {
+  var groupId = $('#form').find('input[name="groupId"]').val();
+  var artifactId = $('#form').find('input[name="artifactId"]').val();
+  var version = $('#form').find('input[name="version"]').val();
+  var className = $('#form').find('input[name="className"]').val();
+
+  if (! /\S/.test(groupId)) {
+    groupId = "org.acme.quarkus.sample";
+  }
+  if (! /\S/.test(artifactId)) {
+    artifactId = "my-quarkus-project";
+  }
+  if (! /\S/.test(version)) {
+    version = "1.0-SNAPSHOT";
+  }
+  if (! /\S/.test(className)) {
+    className = "org.acme.quarkus.sample.HelloResource";
+  }
+
+  var extensions = [];
+  $.each($("#form .extensions option:selected"), function(){            
+      extensions.push($(this).val());
+  });
+
   try {
     let zip = new JSZip();
 
